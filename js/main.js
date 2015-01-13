d3b61b2252577b20d4424e34ac8f931526393dc5
willm <willmurdoch@gmail.com>
2015-01-12 20:36:08 -0500

    It's a working plugin!

--- js/main.js
+++ js/main.js
@@ -1,11 +1,19 @@
 $(document).ready(function(){
-	$('#spot1').wuSlide();
+	$('.slide').wuSlide({
+		color:"red"
+	});
 });
 /*Wu Slider Core*/
-(function ( $ ) {
-
-	$.fn.wuSlide = function(){
-
+(function($){
+	$.fn.wuSlide = function(options){
+		 var settings = $.extend({
+			// These are the defaults.
+			color: "#556b2f",
+		}, options );
+		$(this).css('background',settings.color);
+		console.log(this);
+		 return this.css({
+			color: settings.backgroundColor
+		});
 	};
-	
-}( jQuery ));
+}(jQuery));
