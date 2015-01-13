b00d70f080aa39cadadd6f4bcfa9a05431311243
willm <willmurdoch@gmail.com>
2015-01-12 21:24:57 -0500

    Grid laid out

--- js/main.js
+++ js/main.js
@@ -1,19 +1,33 @@
 $(document).ready(function(){
-	$('.slide').wuSlide({
-		color:"red"
+	$('#spot1').wuSlide({
+		background:"gold"
 	});
 });
+var wuSlide;
 /*Wu Slider Core*/
 (function($){
+	
+	var wuSlide,slideWidth,slideHeight,settings;
 	$.fn.wuSlide = function(options){
-		 var settings = $.extend({
+		var slide = this.children();
+		var slideWidth = slide.outerWidth()-1;
+		var slideHeight = slide.outerHeight();
+		var settings = $.extend({
 			// These are the defaults.
-			color: "#556b2f",
+			background: "red",
 		}, options );
-		$(this).css('background',settings.color);
-		console.log(this);
-		 return this.css({
-			color: settings.backgroundColor
-		});
+		this.wrapInner("<div class='slideWrap'></div>");
+		this.prepend("<div class='slideNav'><div class='lBtn'></div><div class='rBtn'></div></div>");
+		$('.slideWrap').width((slide.outerWidth(true)*(slide.length+1)));
+		/*Apply Options*/
+		slide.width(slideWidth);
+		slide.height(slideHeight);
 	};
+	$(this).parents(window).resize(function(){
+		slide.removeAttr('style');
+		console.log('going');
+		slideWidth = slide.outerWidth()-1;
+		slideHeight = slide.outerHeight();
+		$('.slideWrap').width((slide.outerWidth(true)*(slide.length+1)));
+	});
 }(jQuery));
