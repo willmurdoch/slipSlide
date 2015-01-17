47b9737fddb094ccae8166c58e71d66d2e784e63
willm <willmurdoch@gmail.com>
2015-01-17 16:48:19 -0500

    added clickable option, two focus modes

--- js/slipSlide.js
+++ js/slipSlide.js
@@ -1,15 +1,14 @@
 /*slipSlide Core*/
 (function($){
 	$.fn.slipSlide = function(options){
-		var sStart = 0, sCount = 0, sPos = 0,sOrig;
+		var sStart = 0, sCount = 0, sPos = 0,sOrig,focusSize,focusSlide,currentSlides;
 		var sParent = this;
 		var slide = this.children();
 		
 		/*Settings*/
 		var settings = $.extend({
-			count: 1,
 			focusMode: 1,
-			cycle: true,
+			clickable: true,
 			speed: 500,
 			nav: true
 		}, options );
@@ -29,15 +28,14 @@
 		}
 		else if(settings.nav == true){
 			this.prepend("<div class='slideNav'><div class='lBtn'></div><div class='rBtn'></div></div>");
-			var stopSpam = false;
+			var stopSpam = false,endSlide = false;
 			slideCheck();
 			slideFocus();
 			var sPos = slide.first().position().left;
 			this.find('.slideNav div').click(function(e){
 				var dir = '';
 				if(stopSpam == false){
-				
-					/*Figure out the focus, and where it should move to*/
+					/*Figure out which button you clicked, the focus, and where it should move to*/
 					stopSpam = true;
 					var myFocus = sParent.find('.focus');
 					if($(this).hasClass('lBtn')){
@@ -51,17 +49,37 @@
 					
 					/*If there's another slide to bring in*/
 					if(nextFocus.length){
+						endSlide = false;
 						myFocus.removeClass('focus');
 						nextFocus.addClass('focus');
-						if(!nextFocus.hasClass('current')){
+						if((!nextFocus.hasClass('current')) && (settings.focusMode == 2)){
 							if(dir == 'left') sPos = - nextFocus.position().left;
 							else sPos = - sWrap.find('.current').first().next().position().left;
+						}
+						if(settings.focusMode == 2){
+							if(nextFocus.hasClass('current')){
+								if (sCount > 2){
+									if(dir == 'left'){
+										if(slide.first().offset().left < - slide.first().width()){
+											sPos = - nextFocus.prev().position().left;
+										}
+									}
+									else{
+										if(slide.last().offset().left > sWrap.width()){
+											sPos = - nextFocus.prev().position().left;
+										}
+									}
+								}
+							}
+							
 						}
 					}
 					/*If there are no more slides*/
 					else{
+						endSlide = true;
 						myFocus.removeClass('focus');
 						if(dir == 'left'){
+							/*Move to last slide*/
 							if(myFocus.siblings('.current').length >= 1){
 								nextFocus = slide.last();
 								sPos = - (nextFocus.position().left - (sParent.find('.slideSize').width() -nextFocus.outerWidth(true)));
@@ -72,10 +90,16 @@
 							}
 						}
 						else{
+							/*Move to first slide*/
 							nextFocus = slide.first();
 							sPos = 0;
 						}
-						nextFocus.addClass('focus');
+						if(settings.focusMode == 1){
+							nextFocus.addClass('focus');
+						}
+						else{
+							nextFocus.addClass('focus');
+						}
 					}
 					
 					/*Animate wrapper then re-check visible elements*/
@@ -86,7 +110,22 @@
 				}
 			});
 		} 
-		
+		if(settings.clickable == true){
+			slide.click(function(){
+				if(stopSpam == false){
+					sWrap.find('.focus').removeClass('focus');
+					$(this).addClass('focus');
+					sPos = - $(this).position().left;
+					if(slide.last().offset().left > sWrap.outerWidth(true)){
+						stopSpam = true;
+						sWrap.animate({'left': sPos +'px'},settings.speed,function(){
+							slideCheck();
+							stopSpam = false;
+						});
+					}
+				}
+			});
+		}
 		/*Resize*/
 		var sOrig;
 		$(window).resize(function(){
@@ -94,6 +133,7 @@
 			slideWidth = 0;
 			sWrap.css('left', -(sWrap.find('.current').first().position().left));
 			if((sWrap.find('.focus').offset().left < 0) || (sWrap.find('.focus').offset().left > (sWrap.width() - 1))){
+				sPos = - sWrap.find('.focus').position().left;
 				sWrap.css('left', -(sWrap.find('.focus').position().left));
 			}
 			slide.each(function(){
@@ -117,24 +157,22 @@
 		
 		/*Determine which element gets the focus class on load*/
 		function slideFocus(){
-			var focusSlide = 0;
-			if(settings.focusMode == 1){
-				if($('.slideWrap .current').length > 1){
-					$('.slideWrap .current').first().addClass('focus');
+			focusSlide = 0;
+			currentSlides = sWrap.find('.current');
+			focusSize = currentSlides.length;
+			if(focusSize > 1){
+				if(settings.focusMode == 1){
+					currentSlides.first().addClass('focus');
 				}
 				else{
-					$('.slideWrap .current').addClass('focus');
+					focusSlide = (focusSize/2).toFixed(0) - 1;
+					currentSlides.eq(focusSlide).addClass('focus');
 				}
 			}
 			else{
-				if($('.slideWrap .current').length > 1){
-					focusSlide = ($('.slideWrap .current').length/2).toFixed(0) - 1;
-					$('.slideWrap .current').eq(focusSlide).addClass('focus');
-				}
-				else{
-					$('.slideWrap .current').addClass('focus');
-				}
+				currentSlides.addClass('focus');
 			}
+			
 		}
 	};
 }(jQuery));
